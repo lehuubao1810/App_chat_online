c5b382581b0bd2bb834bae02e5e570ded90d1bba
Huu Bao <lehuubao2909@gmail.com>
2021-07-01 09:59:24 +0700

    upload folder

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MyStack from './navigation';
+import Home from './screens/home';
+import Quiz from './screens/quiz';
+import Result from './screens/result';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return ( <
+        NavigationContainer >
+        <
+        MyStack / >
+        <
+        /NavigationContainer>
+    );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        paddingTop: 40,
+        paddingHorizontal: 16,
+    },
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+# App_chat_online
